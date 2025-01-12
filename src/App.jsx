e10c38958ae12ff2f594a500e2f84d2c6e6baaa2
sandeep limbure <127886236+SandeepLimbure@users.noreply.github.com>
2025-01-12 15:26:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
@@ -18,7 +17,7 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To SUBRAT Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
          
@@ -40,6 +39,3 @@ function App() {
 }
 
 export default App;
-
-
-
